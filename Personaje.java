96eda97550c5718820868b1668b583c371eca0ac
luis211275 <luislopeznunosanchez@gmail.com>
2025-12-02 14:32:57 +0100

    Funcionalidad añadida

--- Personaje.java
+++ Personaje.java
@@ -5,10 +5,31 @@ public class Personaje {
  this.nom = nom;
  this.dinero = dinero;
  }
+ public int getNom(){
+    return nom;
+ }
+ public void setNom(int nom){
+    this.nom;
+ }
+  public int getDinero(){
+    return dinero;
+ }
+ public void setDinero(int dinero){
+    this.dinero;
+ }
  public void mostrarInfo() {
  System.out.println(nom + " tiene " + dinero + "$.");
+ }
+ public void GastarDinero(){
+    private static Scanner sc = new Scanner(System.in);
+    System.out.println("Cuanto dinero te quieres hastar en la tienda de ropa?");
+    int gasto = sc.nextint();
+    setDinero(getDinero-gasto);
+    System.out.println(getNom + "se ha gastado" + gasto + "ahora tiene " + getDinero + "euros");
+    
  }
  public static void main(String[] args) {
  Personaje jugador = new Personaje("Trevor", 500);
  jugador.mostrarInfo();
+ jugador.GastarDinero();
  }
